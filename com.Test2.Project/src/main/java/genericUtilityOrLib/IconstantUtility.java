8e34c36a89923bf0117f9c45f051f810487fddd1
Jigyasha <jigyashachoudhary18@gmail.com>
2024-09-01 01:05:42 +0530

    Add files via upload
    final commit

---
+++ com.Test2.Project/src/main/java/genericUtilityOrLib/IconstantUtility.java
@@ -0,0 +1,9 @@
+package genericUtilityOrLib;
+
+
+public interface IconstantUtility {
+	
+	public String loginpropertiesPath = "D:\\Assignment\\ass.properties";
+	public String filePath = "D:\\Assignment\\demo-data.xlsx";
+
+}
